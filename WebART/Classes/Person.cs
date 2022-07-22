b16d3d7f5e5b33d78964031919ce4c1bf1afae56
artem1607 <artem.kulishki@mail.ru>
2022-07-22 17:58:25 +0700

    ssssss

---
+++ WebART/Classes/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebART.Classes
+{
+    public class Person
+    {
+        public string name;
+        public string name2;
+        public string nickname;
+        public string password;
+    }
+}
