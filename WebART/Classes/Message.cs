bd570de4ea8c30da3fe5af75059ff2a116186f5b
artem1607 <artem.kulishkin@mail.ru>
2022-07-25 17:49:04 +0700

    ввввв

---
+++ WebART/Classes/Message.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebART.Classes
+{
+    public class Message
+    {
+        private string message = "";
+        private string user = "";
+        private string date = "";
+        public void setDate()
+        {
+            date = System.DateTime.Now.Hour.ToString() + " часов " + System.DateTime.Now.Minute.ToString() + " минут";
+        }
+        public void setUser(string name)
+        {
+            user = name;
+        }
+        public void setMessage(string msg)
+        {
+            message = msg;
+        }
+
+        public string getMessage()
+        {
+            return user + ": " + message;
+        }
+        public string getDate()
+        {
+            return date;
+        }
+    }
+}
